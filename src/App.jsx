47ba29c57ade49c8131439b064c803ee8dcf55ef
Jianru D <dengjr1509@gmail.com>
2024-03-26 15:15:20 -0700

    setup and try css

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ import "./App.css";
 import "tailwindcss/tailwind.css";
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <Router>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-lx hover:bg-light-gray
+                 text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
         </div>
       </Router>
     </div>
