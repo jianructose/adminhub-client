4f44d265e65bcfee037940c432e7f571597b960b
jianructose <70822647+jianructose@users.noreply.github.com>
2024-04-01 02:52:18 +0000

    add axios call

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -14,13 +14,32 @@ import { registerLicense } from "@syncfusion/ej2-base";
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekxwWmFZfVpgdV9EZFZSR2Y/P1ZhSXxXdkZgWX5dcnBWRWNbWUM="
 );
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Header from "../components/Header";
 
-// get json data to array
+const ApiUrl =
+  "https://turbo-space-robot-wxjpjqwqqqr3gvvw-8080.app.github.dev/api/events";
+// async function to fetch scheduleData
+
 import eventData from "../data/events.json";
 
 const Calendar = () => {
+  // get json data to array
+  const [scheduleData, setScheduleData] = useState([]);
+  const fetchScheduleData = async () => {
+    try {
+      const response = await axios.get(ApiUrl);
+      setScheduleData(response.data);
+    } catch (error) {
+      console.error("Error fetching schedule data: ", error);
+    }
+  };
+  useEffect(() => {
+    fetchScheduleData();
+    console.log("fetching schedule data, scheduleData: ", scheduleData);
+  }, []);
   return (
     <main className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-slate-100 rounded-3xl">
       <Header title="Calendar" />
