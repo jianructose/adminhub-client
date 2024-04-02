01cef6291e8d9c38355a219a29f2ebc8284a04fb
Jianru D <dengjr1509@gmail.com>
2024-04-01 18:20:33 -0700

    add crud data mgr

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -14,8 +14,15 @@ import { registerLicense } from "@syncfusion/ej2-base";
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekxwWmFZfVpgdV9EZFZSR2Y/P1ZhSXxXdkZgWX5dcnBWRWNbWUM="
 );
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import {
+  DataManager,
+  WebApiAdaptor,
+  UrlAdaptor,
+  WebMethodAdaptor,
+  ODataAdaptor,
+} from "@syncfusion/ej2-data";
 
 import Header from "../components/Header";
 
@@ -23,38 +30,48 @@ const baseAPI = "http://localhost:8080/api/";
 // async function to fetch scheduleData
 
 import eventData from "../data/events.json";
+import { Button } from "../components";
 
 const Calendar = () => {
   // get json data to array
-  const [scheduleData, setScheduleData] = useState([]);
-  const fetchScheduleData = async () => {
-    try {
-      const response = await axios.get(`${baseAPI}events`);
+  // const [scheduleData, setScheduleData] = useState([]);
+  // const fetchScheduleData = async () => {
+  //   try {
+  //     const response = await axios.get(`${baseAPI}events`);
 
-      // convert time to local time
+  //     setScheduleData(response.data);
+  //     setTimeout(() => {
+  //       console.log("Schedule Data: ", scheduleData);
+  //     }, 3000);
+  //   } catch (error) {
+  //     console.error("Error fetching schedule data: ", error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchScheduleData();
+  // }, []);
 
-      response.data.forEach((event) => {
-        event.StartTime = new Date(event.StartTime);
-        event.EndTime = new Date(event.EndTime);
-      });
+  const dataManager = new DataManager({
+    adaptor: new UrlAdaptor(),
+    url: `${baseAPI}events`,
+    insertUrl: `${baseAPI}events/insert`,
+    removeUrl: `${baseAPI}events/delete`,
+    updateUrl: `${baseAPI}events/update`,
+    crossDomain: true, // enable this if you are using CORS
+  });
 
-      console.log("fetching schedule data, scheduleData: ", response.data);
-      setScheduleData(response.data);
-    } catch (error) {
-      console.error("Error fetching schedule data: ", error);
-    }
-  };
-  useEffect(() => {
-    fetchScheduleData();
-  }, []);
+  const scheduleObj = useRef(null); // create a ref for the ScheduleComponent
+
   return (
     <main className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-slate-100 rounded-3xl">
       <Header title="Calendar" />
       <ScheduleComponent
         height={550}
-        eventSettings={{ dataSource: scheduleData }}
+        eventSettings={{ dataSource: dataManager }}
         selectedDate={new Date()}
         currentView="Month"
+        timezone="America/Los_Angeles"
+        ref={scheduleObj}
       >
         <Inject
           services={[
